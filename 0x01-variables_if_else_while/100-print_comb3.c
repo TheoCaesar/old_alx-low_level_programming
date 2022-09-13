0e9a18943768533f1a54a299391043bb5d0cf9bd
TheoCaesar <theocaesar@gmail.com>
2022-09-12 22:46:42 -0700

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - adv
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	while (a < 100)
+	{
+		b = a % 10;
+		c = a % 10;
+
+		if (c < b)
+		{
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

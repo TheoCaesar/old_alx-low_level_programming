a8ebe946ac60787c50f6bedb5596455d9dcc851b
TheoCaesar <theocaesar@gmail.com>
2022-09-12 22:33:33 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quot, 59);
+	return (1);
+}
